152affbee0fda301b3abe48f670aa9d54e83bcc4
Radoslav Radev <radoslavradev@Radoslavs-MacBook-Air.local>
2022-07-08 16:51:07 +0300

    Voting

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,73 +1,65 @@
-   // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-
 
 import "./IVoting.sol";
 
 contract Voting is IVoting {
+    Parties public winner;
 
+    address public owner;
 
-Parties public winner;
+    uint private constant DURATION = 360 seconds;
+    uint public startAt;
+    uint public endAt;
 
-address public owner;
+    mapping(uint => uint) partycount;
+    mapping(address => bool) private voted;
 
-uint constant private DURATION = 360 seconds;
-uint public startAt;
-uint public endAt;
-
-mapping (uint => uint) partycount;
-mapping (address => bool) private voted;
-
-
-constructor() {
-     startAt = block.timestamp;
-     endAt = block.timestamp + DURATION;
-     owner = msg.sender;
-}
-
-modifier cantVoteTwice {
-    require(voted[msg.sender] == false, "You cannot vote twice");
-    _;
-}
-
-
-function vote(Parties _party) external override cantVoteTwice {
-require(endAt > block.timestamp, "Voting has finished");
-voted[msg.sender] = true;
-if(_party == Parties.A) {
-    partycount[uint(Parties.A)] += 1;
-emit VotedforA();
-} else if (_party == Parties.B)  {
-    partycount[uint(Parties.B)] += 1;
-emit VotedforB();
-} else if (_party == Parties.C) {
-     partycount[uint(Parties.C)] += 1;
-emit VotedforC();
-}
-}
-
-
-  
-function getWinner() external override returns(Parties) {
-require(block.timestamp > endAt, "Voting hasn't finished yet");
- 
-if(partycount[uint(Parties.A)] > partycount[uint(Parties.B)] && partycount[uint(Parties.A)] > partycount[uint(Parties.C)]) {
-winner = Parties.A;
- } 
- else if(partycount[uint(Parties.B)] > partycount[uint(Parties.A)] && partycount[uint(Parties.B)] > partycount [uint(Parties.C)]) {
-     winner = Parties.B;
-    }
-else {
-     winner = Parties.C;
+    constructor() {
+        startAt = block.timestamp;
+        endAt = block.timestamp + DURATION;
+        owner = msg.sender;
     }
 
-  
-emit Winner();
+    modifier cantVoteTwice() {
+        require(voted[msg.sender] == false, "You cannot vote twice");
+        _;
+    }
 
-return winner;
+    function vote(Parties _party) external override cantVoteTwice {
+        require(endAt > block.timestamp, "Voting has finished");
+        voted[msg.sender] = true;
+        if (_party == Parties.A) {
+            partycount[uint(Parties.A)] += 1;
+            emit VotedforA();
+        } else if (_party == Parties.B) {
+            partycount[uint(Parties.B)] += 1;
+            emit VotedforB();
+        } else if (_party == Parties.C) {
+            partycount[uint(Parties.C)] += 1;
+            emit VotedforC();
+        }
+    }
 
+    function getWinner() external override returns (Parties) {
+        require(block.timestamp > endAt, "Voting hasn't finished yet");
+
+        if (
+            partycount[uint(Parties.A)] > partycount[uint(Parties.B)] &&
+            partycount[uint(Parties.A)] > partycount[uint(Parties.C)]
+        ) {
+            winner = Parties.A;
+        } else if (
+            partycount[uint(Parties.B)] > partycount[uint(Parties.A)] &&
+            partycount[uint(Parties.B)] > partycount[uint(Parties.C)]
+        ) {
+            winner = Parties.B;
+        } else {
+            winner = Parties.C;
+        }
+
+        emit Winner();
+
+        return winner;
+    }
 }
-
-}
-
-
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("solidity-coverage");
+import "@nomiclabs/hardhat-solhint";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
